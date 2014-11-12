51fb0bd70047bfb8eb1820fb6bf4bb08b860ffb0
Pierre Poissinger <Pierre.Poissinger@ordina.be>
2014-11-12 12:00:27 +0100

    Initial commit - project for integration tests based on JBOSS mdb quick start

---
+++ src/main/java/be/ordina/inttest/mdb/SuperQueueMDB.java
@@ -0,0 +1,41 @@
+package be.ordina.inttest.mdb;
+
+import java.util.logging.Logger;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * <p>
+ * A simple Message Driven Bean that asynchronously receives and processes the messages that are sent to the queue.
+ * </p>
+ * 
+ */
+@MessageDriven(name = "SuperQueueMDB", activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/SuperQueue"),
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
+public class SuperQueueMDB implements MessageListener {
+
+    private final static Logger LOGGER = Logger.getLogger(SuperQueueMDB.class.toString());
+
+    /**
+     * @see MessageListener#onMessage(Message)
+     */
+    public void onMessage(Message rcvMessage) {
+        TextMessage msg = null;
+        try {
+            if (rcvMessage instanceof TextMessage) {
+                msg = (TextMessage) rcvMessage;
+                LOGGER.info("Received Message from queue: " + msg.getText());
+            } else {
+                LOGGER.warning("Message of wrong type: " + rcvMessage.getClass().getName());
+            }
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
